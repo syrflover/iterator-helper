9d9a650d1e2eb2d23ae5097cffcd281ebc2ca24f
Lee Tae Woo <syr@meu.works>
2019-10-23 04:23:30 +0900

    change test count

--- test/iterator/count.test.ts
+++ test/iterator/count.test.ts
@@ -1,12 +1,12 @@
-import '../../src';
-
 import { assert } from 'chai';
+
+import { IteratorHelper } from '../../src';
 
 describe('test count', () => {
     it('[1,2,3,4,5].length === count()', () => {
         const a = [1, 2, 3, 4, 5];
 
-        const actual = a.iter().count();
+        const actual = new IteratorHelper([1, 2, 3, 4, 5]).count();
         const expected = a.length;
 
         assert.strictEqual(actual, expected);
