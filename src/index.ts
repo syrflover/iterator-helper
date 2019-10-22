e333e0804f6da70dd454eec614deec0a6bf0a969
Lee Tae Woo <syr@meu.works>
2019-10-23 02:37:29 +0900

    impl Array.prototype.iter()

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import { IterableHelper } from './iterable';
+import './types/array';
 import { logger } from './logger';
 
-const a = new IterableHelper([1, 2, 3, 4]).map((e) => e + 1).collect();
-logger.debug(a);
+export { IteratorHelper } from './iterator';
+
+/* const a = [1, 2, 3, 4, 5, 6, 7, 8].iter();
+
+a.filter((e) => e % 2 === 0)
+    .map((e) => e + 1)
+    .collect(); */
--- src/types/array.ts
+++ src/types/array.ts
@@ -1,1 +1,21 @@
+import { IteratorHelper } from '../iterator';
+
 // TODO array [].iter() impl
+declare global {
+    // tslint:disable-next-line: interface-name
+    interface Array<T> {
+        iter(): IteratorHelper<T>;
+    }
+}
+
+Array.prototype.iter = function () {
+    // tslint:disable-next-line: no-this-assignment
+    const self = this;
+    const it = function* () {
+        yield* self;
+    };
+
+    return new IteratorHelper(it());
+};
+
+export = global;
