316bf151e68cd9c603fdfa835a10bd7a07fa65bd
Lee Tae Woo <syr@meu.works>
2019-10-23 21:05:35 +0900

    Update eslint prettier

--- src/iterator/find.ts
+++ src/iterator/find.ts
@@ -1,9 +1,6 @@
 import { PredicateFn } from '../types/fn/predicate';
 
-export async function _find<T>(
-    iter: AsyncIterable<T>,
-    predicate: PredicateFn<T>,
-) {
+export async function _find<T>(iter: AsyncIterable<T>, predicate: PredicateFn<T>) {
     for await (const elem of iter) {
         if (await predicate(elem)) {
             return elem;
--- src/logger.ts
+++ src/logger.ts
@@ -2,4 +2,5 @@ import { getLogger } from 'log4js';
 
 export const logger = getLogger('iterator-helper');
 
+/* eslint no-undef: "off" */
 logger.level = globalThis?.process?.env?.ITER_HELPER_LOG_LEVEL ?? '';
