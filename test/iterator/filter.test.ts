eb7c6cb8cf2c31ca252f86b07a78a0c2a86881cc
Lee Tae Woo <syr@meu.works>
2019-10-23 19:52:54 +0900

    Fix test filter

--- test/iterator/filter.test.ts
+++ test/iterator/filter.test.ts
@@ -4,7 +4,7 @@ import { IteratorHelper } from '../../src';
 
 describe('test filter', () => {
     it('even', async () => {
-        const a = [1, 2, 3, 4, 5, 6];
+        const a = new IteratorHelper([1, 2, 3, 4, 5, 6]);
 
         const actual: number[] = [];
         const expected = [2, 4, 6];
