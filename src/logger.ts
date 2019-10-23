010554478bd3fe14cb3ea109ba62c6ea017438b1
Lee Tae Woo <syr@meu.works>
2019-10-23 20:07:32 +0900

    Update logger.ts

--- src/logger.ts
+++ src/logger.ts
@@ -2,10 +2,4 @@ import { getLogger } from 'log4js';
 
 export const logger = getLogger('iterator-helper');
 
-if (
-    globalThis.process &&
-    globalThis.process.env &&
-    globalThis.process.env.ITER_HELPER_LOG_LEVEL
-) {
-    logger.level = globalThis.process.env.ITER_HELPER_LOG_LEVEL;
-}
+logger.level = globalThis?.process?.env?.ITER_HELPER_LOG_LEVEL ?? '';
