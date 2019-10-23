de56f3a7185e6a52310afa0897f755fb02e1e7be
Lee Tae Woo <syr@meu.works>
2019-10-23 20:45:47 +0900

    Change tslint to eslint

--- src/array/iter.ts
+++ src/array/iter.ts
@@ -1,17 +1,14 @@
 import { IteratorHelper } from '../iterator';
 
-// TODO array [].iter() impl
 declare global {
-    // tslint:disable-next-line: interface-name
     interface Array<T> {
         iter(): IteratorHelper<T>;
     }
 }
 
-Array.prototype.iter = function () {
-    // tslint:disable-next-line: no-this-assignment
+Array.prototype.iter = function() {
     const self = this;
-    const it = function* () {
+    const it = function*() {
         yield* self;
     };
 
