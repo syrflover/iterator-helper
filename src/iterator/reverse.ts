6dd1a82c70d2c49fffb3a626bf5cbd8218155679
Lee Tae Woo <syr@meu.works>
2019-10-27 21:32:44 +0900

    change iterator/reverse impl

--- src/iterator/reverse.ts
+++ src/iterator/reverse.ts
@@ -11,13 +11,13 @@ import { prepend } from '../lib/iterable/prepend';
 
 const logger = getLogger('iterator/reverse');
 
-function _reverse_impl_fn<T>(iter: AsyncIterable<T>) {
+async function* _reverse_impl_fn<T>(iter: AsyncIterable<T>) {
     logger.trace('_reverse_impl_fn()');
     const emptyIter = toAsyncIterable<T>([]);
-    return _foldl((acc, e) => flip(prepend, acc, e), emptyIter, iter);
+    yield* await _foldl((acc, e) => flip(prepend, acc, e), emptyIter, iter);
 }
 
 export function _reverse<T>(iter: AsyncIterable<T>) {
     logger.trace('_reverse()');
-    return toAsyncIterable(_reverse_impl_fn(iter));
+    return _reverse_impl_fn(iter);
 }
