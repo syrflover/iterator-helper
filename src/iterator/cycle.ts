879c48e44427f3681bb45f5285df9cddbbce1f55
Lee Tae Woo <syr@meu.works>
2019-11-01 03:29:19 +0900

    Change iterator/cycle impl

--- src/iterator/cycle.ts
+++ src/iterator/cycle.ts
@@ -3,10 +3,11 @@ import { getLogger } from '../logger';
 import { toAsyncIterable } from '../lib/iterable';
 
 import { next_async } from '../lib/iterable/next';
+import { append } from '../lib/iterable/append';
 
 const logger = getLogger('iterator/cycle');
 
-async function* _cycle_impl_fn<T>(iter: AsyncIterable<T>, r: T[] = []): AsyncIterable<T> {
+async function* _cycle_impl_fn<T>(iter: AsyncIterable<T>, r: AsyncIterable<T> = toAsyncIterable([])): AsyncIterable<T> {
     logger.trace('_cycle_impl_fn()');
     const { done, value } = await next_async(iter);
 
@@ -15,11 +16,11 @@ async function* _cycle_impl_fn<T>(iter: AsyncIterable<T>, r: T[] = []): AsyncIte
 
     if (!done) {
         yield value;
-        yield* _cycle_impl_fn(iter, [...r, value]);
+        yield* _cycle_impl_fn(iter, append(value, r));
         return;
     }
 
-    yield* _cycle_impl_fn(toAsyncIterable(r));
+    yield* _cycle_impl_fn(r);
 }
 
 export function _cycle<T>(iter: AsyncIterable<T>) {
