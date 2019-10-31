0409a348bcc41d37f629bab0466210732d7661f3
Lee Tae Woo <syr@meu.works>
2019-11-01 03:32:58 +0900

    Change iterator/zip impl

--- src/iterator/zip.ts
+++ src/iterator/zip.ts
@@ -1,17 +1,16 @@
 import { getLogger } from '../logger';
 
 import { Pair, pair } from '../types/pair';
-import { isArrayLike } from '../types/guard/isArrayLike';
 
-import { toIterable } from '../lib/iterable';
-import { next_async, next } from '../lib/iterable/next';
+import { toAsyncIterable } from '../lib/iterable';
+import { next_async } from '../lib/iterable/next';
 
 const logger = getLogger('iterator/zip');
 
-async function* _zip_impl_fn<T, U>(iter: AsyncIterable<T>, other: Iterable<U> | AsyncIterable<U>): AsyncIterable<Pair<T, U>> {
+async function* _zip_impl_fn<T, U>(iter: AsyncIterable<T>, other: AsyncIterable<U>): AsyncIterable<Pair<T, U>> {
     logger.trace('_zip_impl_fn()');
     const { done: iter_done, value: iter_value } = await next_async(iter);
-    const { done: other_done, value: other_value } = await next(other);
+    const { done: other_done, value: other_value } = await next_async(other);
 
     if (iter_done || other_done) {
         return;
@@ -24,6 +23,6 @@ async function* _zip_impl_fn<T, U>(iter: AsyncIterable<T>, other: Iterable<U> | 
 
 export function _zip<T, U>(other: Iterable<U> | AsyncIterable<U>, iter: AsyncIterable<T>) {
     logger.trace('_zip()');
-    const other_ = isArrayLike(other) ? toIterable(other) : other;
+    const other_ = toAsyncIterable(other);
     return _zip_impl_fn(iter, other_);
 }
