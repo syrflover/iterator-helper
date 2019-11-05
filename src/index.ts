9d2e000788a864582a994fdf86b01e53fc469067
Lee Tae Woo <syr@meu.works>
2019-11-05 14:14:38 +0900

    export *

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,83 @@ export {
     ToAsyncIterator,
     IAsyncIterator_,
     IAsyncIterator_number,
+    IAsyncIterator_zip,
     iterator,
 } from './iterator';
+
+// methods
+export { _all as all } from './methods/all';
+export { _any as any } from './methods/any';
+export { _average as average } from './methods/average';
+export { _chain as chain } from './methods/chain';
+export { _collect as collect } from './methods/collect';
+export { _count as count } from './methods/count';
+export { _cycle as cycle } from './methods/cycle';
+export { _enumerate as enumerate } from './methods/enumerate';
+export { _filter as filter } from './methods/filter';
+export { _filterMap as filterMap } from './methods/filterMap';
+export { _find as find } from './methods/find';
+export { _findMap as findMap } from './methods/findMap';
+export { _flatMap as flatMap } from './methods/flatMap';
+export { _flatten as flatten } from './methods/flatten';
+export { _foldl as foldl } from './methods/foldl';
+export { _foldl1 as foldl1 } from './methods/foldl1';
+export { _forEach as forEach } from './methods/forEach';
+export { _head as head } from './methods/head';
+export { _inspect as inspect } from './methods/inspect';
+export { _last as last } from './methods/last';
+export { _map as map } from './methods/map';
+export { _max as max } from './methods/max';
+export { _maxBy as maxBy } from './methods/maxBy';
+export { _maxByKey as maxByKey } from './methods/maxByKey';
+export { _min as min } from './methods/min';
+export { _minBy as minBy } from './methods/minBy';
+export { _minByKey as minByKey } from './methods/minByKey';
+export { _nth as nth } from './methods/nth';
+export { _nub as nub } from './methods/nub';
+export { _nubBy as nubBy } from './methods/nubBy';
+export { _partition as partition } from './methods/partition';
+export { _position as position } from './methods/position';
+export { _product as product } from './methods/product';
+export { _reverse as reverse } from './methods/reverse';
+export { _scanl as scanl } from './methods/scanl';
+export { _scanl1 as scanl1 } from './methods/scanl1';
+export { _scanr as scanr } from './methods/scanr';
+export { _scanr1 as scanr1 } from './methods/scanr1';
+export { _skip as skip } from './methods/skip';
+export { _skipWhile as skipWhile } from './methods/skipWhile';
+export { _stepBy as stepBy } from './methods/stepBy';
+export { _sum as sum } from './methods/sum';
+export { _take as take } from './methods/take';
+export { _takeWhile as takeWhile } from './methods/takeWhile';
+export { _foldr as foldr } from './methods/foldr';
+export { _foldr1 as foldr1 } from './methods/foldr1';
+export { _unzip as unzip } from './methods/unzip';
+export { _zip as zip } from './methods/zip';
+
+// types
+export * from './types/flatten';
+export * from './types/nullable';
+export * from './types/ordering';
+export * from './types/pair';
+export * from './types/promise';
+
+// fn types
+export * from './types/fn/byKey';
+export * from './types/fn/cmp';
+export * from './types/fn/equal';
+export * from './types/fn/fold';
+export * from './types/fn/forEach';
+export * from './types/fn/map';
+export * from './types/fn/predicate';
+export * from './types/fn/scan';
+
+// iterable utils
+export * from './lib/iterable';
+export * from './lib/iterable/append';
+export * from './lib/iterable/next';
+export * from './lib/iterable/prepend';
+
+// utils
+export * from './lib/cmp';
+export * from './lib/flip';
