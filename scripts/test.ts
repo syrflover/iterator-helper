cbd6f02bfb0d48cedfc4ede28c910eabf6764fdd
Lee Tae Woo <syr@meu.works>
2020-05-17 13:13:48 +0900

    Update scripts/test.ts

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,22 +1,23 @@
 /* eslint-disable */
-const entries: string[] = [];
+import { $ } from '../src/lib/utils/mod.ts';
+import { collect } from '../src/methods/mod.ts';
 
-async function main(dir: string) {
-    const ex = ['src/playground.ts', 'src/logger.ts', 'src/iterator.ts'];
+async function* getFileNamesInDirectory(dir: string): AsyncIterable<string> {
+    const excludes = ['src/playground.ts', 'src/logger.ts', 'src/iterator.ts'];
 
     const files = Deno.readDir(dir);
 
     for await (const file of files) {
         try {
-            if (ex.includes(`${dir}/${file.name}`)) {
+            if (excludes.includes(`${dir}/${file.name}`)) {
                 continue;
             }
 
             const stat = await Deno.stat(`${dir}/${file.name}`);
             if (stat.isFile) {
-                entries.push(`${dir}/${file.name}`);
+                yield `${dir}/${file.name}`;
             } else if (stat.isDirectory) {
-                await main(`${dir}/${file.name}`);
+                yield* getFileNamesInDirectory(`${dir}/${file.name}`);
             }
         } catch (error) {
             console.error(file.name, error);
@@ -24,55 +25,65 @@ async function main(dir: string) {
     }
 }
 
-main('src')
-    .then(async () => {
-        const ex = [/src\/types\/[a-z]+\.ts/i, /src\/types\/functions\/[a-z]+\.ts/i];
+async function filterSourceFiles(files: string[]) {
+    // const excludes = ['src/types/']; // [/src\/types\/[a-z]+\.ts/i, /src\/types\/functions\/[a-z]+\.ts/i];
 
-        const sources: string[] = [];
-        const tests = entries.filter((e) => e.endsWith('_test.ts'));
-
-        pushToSources: for await (const e of entries) {
-            if (e.endsWith('mod.ts')) {
-                continue;
-            }
-
-            if (e.endsWith('_test.ts')) {
-                continue;
-            }
-
-            for (const r of ex) {
-                if (r.test(e)) {
-                    continue pushToSources;
-                }
-            }
-
-            sources.push(e);
+    return files.filter((file) => {
+        if (file.endsWith('mod.ts')) {
+            return false;
         }
 
-        checkTestFile: for (const source of sources) {
-            const withTestExt = source.replace(/\.ts$/, '_test.ts');
-
-            for (const test of tests) {
-                if (test.includes(withTestExt)) {
-                    continue checkTestFile;
-                }
-            }
-
-            console.error(`Not Found Test of "${source}"`);
-            Deno.exit(1);
+        if (file.endsWith('_test.ts')) {
+            return false;
         }
 
-        return tests;
-    })
-    .then(async (tests) => {
-        const reload = Deno.args.includes('reload') ? ' --reload' : '';
-        const testOpts = `--allow-net${reload} --config tsconfig.test.json ${tests.join(' ')}`;
+        // const isExclude = excludes.some((exclude) => exclude.test(file));
 
-        const test = Deno.run({
-            cmd: `deno test ${testOpts}`.split(' '),
-        });
+        if (file.startsWith('src/types/')) {
+            return false;
+        }
 
-        const { code } = await test.status();
-
-        Deno.exit(code);
+        return true;
     });
+}
+
+function hasAllTestFiles(files: string[], testFiles: string[]) {
+    l: for (const file of files) {
+        const withTestExt = file.replace(/\.ts$/, '_test.ts');
+
+        for (const testFile of testFiles) {
+            if (testFile.includes(withTestExt)) {
+                continue l;
+            }
+        }
+
+        console.error(`Not Found Test File of "${file}"`);
+        Deno.exit(1);
+    }
+}
+
+async function runTest(testFiles: string[]) {
+    const reload = Deno.args.includes('reload') ? ' --reload' : '';
+    const testOpts = `--unstable${reload} --config tsconfig.test.json ${testFiles.join(' ')}`;
+
+    const test = Deno.run({
+        cmd: `deno test ${testOpts}`.split(' '),
+    });
+
+    const { code } = await test.status();
+
+    Deno.exit(code);
+}
+
+async function main() {
+    const files = await $(getFileNamesInDirectory('src'), collect);
+
+    const sourceFiles = await filterSourceFiles(files);
+    const testFiles = files.filter((sourceFile) => sourceFile.endsWith('_test.ts'));
+
+    hasAllTestFiles(sourceFiles, testFiles);
+
+    await runTest(testFiles);
+}
+
+main();
