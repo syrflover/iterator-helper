b9bc06c24afc9460253015c872a076dd46f3f447
Lee Tae Woo <syr@meu.works>
2021-05-20 23:44:32 +0900

    export iterator functions

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,5 @@
 export type { AsyncIterator_, ToAsyncIterator, IAsyncIterator_, IAsyncIterator_number, IAsyncIterator_zip } from './iterator.ts';
 
 export { iterator } from './iterator.ts';
+
+export * from './methods/mod.ts';
